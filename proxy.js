491084bfba89a060b0398f3834a5c686c38ca305
武扬 <956826374@qq.com>
2018-12-15 18:14:58 +0800

    修改代理模式

--- proxy.js
+++ proxy.js
@@ -15,38 +15,44 @@ class Secretary{
     }
 }
 const sec = new Secretary(new CEO())
-console.log(sec.getPhoneNo('ceo')) // 188-8888-8888
-console.log(sec.getPhoneNo())   // 010-888888
+// console.log(sec.getPhoneNo('ceo')) // 188-8888-8888
+// console.log(sec.getPhoneNo())   // 010-888888
 
 
 class MVVM{
     constructor(){
-        this.name = 'abc'
-        this.age = 18
-        this.sex = '女'
-        this._weight = 300
+        this.data = {
+            name: '花花',
+            age: 18,
+            sex: '女',
+            _weight: 300
+        }
     }
 }
 
 const mvvm = new Proxy(new MVVM(), {
     get(target, key){
+        const data = target.data;
         if(key[0] === '_'){
             return '不告诉你'
         }else{
-            console.log(`获取了${key}`)
-            return target[key]
+            // console.log(`获取了${key}`)
+            return data[key]
         }
     },
     set(target, key, value){
+        const data = target.data;
         if(key[0] === '_'){
             return '这个你不能设置哦~'
         }else{
-            console.log(`设置了${key}是${value}`)
-            target[key] = value
+            // console.log(`设置了${key}是${value}`)
+            data[key] = value
         } 
     }
 })
+console.log(mvvm)
 console.log(mvvm.name)
 console.log(mvvm._weight)
 mvvm.age = 81
 mvvm._weight = 80
+console.log(mvvm)
